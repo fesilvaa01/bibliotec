ff6975dadb84ab8f8b9b66470865b188605bac32
FELIPE SILVA DE OLIVEIRA <54016391806@info.local>
2026-09-11 17:22:38 -0300

    :iphone:feat Responsividade finalizada

---
+++ home/home.js
@@ -0,0 +1,16 @@
+
+const menu = document.getElementById("menu"); // pegando o elemento pelo ID
+
+//estamos pegando o elemento de forma generica - pode ser classe(precisa trazer o ponto) ou pode ser ID(precisa trazer a hashtag)
+const navMenu = document.querySelector(".navegacao");
+
+menu.addEventListener("click", function() {
+    if(navMenu.className == "navegacao")
+    {
+        navMenu.className = "navegacao ativo";
+    }
+    else
+    {
+        navMenu.className = "navegacao";
+    }
+})
